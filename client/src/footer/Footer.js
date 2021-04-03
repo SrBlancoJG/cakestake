4d97108b771dc541cfa4881e5a4044d3d281ba9a
Alfredo <alfredo.padilla@metrica.es>
2021-04-03 14:18:53 +0200

    Cambios en los enlaces

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -7,16 +7,14 @@ function Footer() {
       <div className="flex-row">
         <div className="flex-col txt-left">
           <p className="sm-txt">Powered by</p>
-          <a href={'https://www.binance.org/en/smartChain'} className="sm-txt">Binance Smart Chain</a>
+          <a href={'https://www.binance.org/en/smartChain'} target={'_blank'} className="sm-txt">Binance Smart Chain</a>
         </div>
 
-        <div className="flex-col txt-center">
-
-        </div>
+        <div className="flex-col txt-center"></div>
 
         <div className="flex-col txt-right">
           <p className="sm-txt">© 2021. All rights reserved.</p>
-          <a href={'https://bnbstake.app'} className="sm-txt">https://bnbfactory.app</a>
+          <a href={'https://bnbstake.app'} target={'_blank'} className="sm-txt">https://bnbfactory.app</a>
         </div>
       </div>
     </div>
